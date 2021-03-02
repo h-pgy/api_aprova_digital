a9b1ef8f8858a14df88dd74a00fefac5db962b8a
h-pgy <hpougy@gmail.com>
2021-03-02 16:04:54 -0300

    ajustes documentos responsavel

--- process_data/responsavel_imovel.py
+++ process_data/responsavel_imovel.py
@@ -20,6 +20,84 @@ def _build_address_respo(respo):
 
     return endereco
 
+def __treat_cep(respo):
+
+    cep = respo.get_m(['cep_proprietario', 'cep'], None)
+
+    if type(cep) is dict:
+        cep = cep.get('input')
+
+    return cep
+
+def __doc_respo_complex(respo, dados):
+
+    doc = respo.get_m(['cpfcnpj_proprietario'])
+
+    dados_doc = [b_resp(
+        'doc',
+        'Número do documento do responsável pelo imóvel',
+        doc.get_m(['cpfCnpj'])
+    ),
+        b_resp(
+            'tipo_doc',
+            'Tipo de documento do responsável pelo imóvel',
+            respo.get_m(doc).get_m(['type'], None))
+    ]
+
+    dados.extend(dados_doc)
+
+    if doc.get_m(['type']) == 'cnpj':
+        dados_responsavel_empresa = [
+            b_resp('nome_resp_empresa',
+                   'Nome do responsável pela empresa',
+                   respo.get_m(['cpfcnpj_proprietario']).get_m(['nomeResp'])),
+
+            b_resp('cpf_resp_empresa',
+                   'CPF do responsável pela empresa',
+                   respo.get_m(['cpfcnpj_proprietario', 'cnpj_proprietario']).get_m(['cpfResp']))
+        ]
+        dados.extend(dados_responsavel_empresa)
+
+def __doc_respo_flat_cnpj(respo, dados):
+
+    dados_doc = [b_resp(
+        'doc',
+        'Número do documento do responsável pelo imóvel',
+        respo.get_m(['cnpj_proprietario'])
+    ),
+        b_resp(
+            'tipo_doc',
+            'Tipo de documento do responsável pelo imóvel',
+            'cnpj')
+    ]
+
+    dados_responsavel_empresa = [
+        b_resp('nome_resp_empresa',
+               'Nome do responsável pela empresa',
+               respo.get_m(['representante_proprietario'])),
+
+        b_resp('cpf_resp_empresa',
+               'CPF do responsável pela empresa',
+               respo.get_m(['cpf_proprietario']))
+    ]
+
+    dados.extend(dados_doc)
+    dados.extend(dados_responsavel_empresa)
+
+def __treat_doc_respo(respo):
+
+    dados = []
+
+    if 'cpfcnpj_proprietario' in respo:
+
+        __doc_respo_complex(respo, dados)
+
+    elif 'cnpj_proprietario' in respo:
+
+        __doc_respo_flat_cnpj(respo, dados)
+
+    return dados
+
 
 def _dados_um_resp(respo):
     '''Parse all the data for one of the owner's info'''
@@ -34,17 +112,6 @@ def _dados_um_resp(respo):
             'Tipo de vínculo do responsável pelo imóvel',
             respo.get_m(['tipo_vinculo_proprietario',
                          'tipo_vinculo'], None)),
-
-        b_resp(
-            'doc',
-            'Número do documento do responsável pelo imóvel',
-            respo.get_m(['cpfcnpj_proprietario']).get_m(['cpfCnpj'], None)),
-
-        b_resp(
-            'tipo_doc',
-            'Tipo de documento do responsável pelo imóvel',
-            respo.get_m(['cpfcnpj_proprietario']).get_m(['type'], None)),
-
         b_resp(
             'email',
             'E-mail do responsável pelo imóvel',
@@ -57,13 +124,15 @@ def _dados_um_resp(respo):
 
         b_resp('cep',
                'CEP do responsável pelo imóvel',
-               respo.get_m(['cep_proprietario'], None)),
+               __treat_cep(respo)),
 
         #NÃO CONSEGUI IDENTIFICAR NENHUM PROCESSO COM INFORMAÇÃO DE TELEFONE DO RESP
         b_resp('telefone',
                'Telefone do responsável pelo imóvel',
                'Informação Não Disponível')
     ]
+
+    dados.extend(__treat_doc_respo(respo))
 
     return dados
 
