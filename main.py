61e3f530317be50339d18a3c52d0e5ef2ff2805e
h-pgy <hpougy@gmail.com>
2021-03-03 08:17:00 -0300

    added new process metadata endpoint and some logging prints

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from typing import Optional
 from utilities.my_tools import get_proc_aleatorio, get_proc
 from utilities.proj_decorators import treat_proc_num_out_of_pattern, treat_proc_not_found
 from process_data.responsavel_imovel import dados_resps_imovel
+from process_data.metadados_processo import get_proc_mdata
 
 app = FastAPI()
 
-@app.get("/responsavel_imovel/{num_proc}")
+@app.get("/responsavel_imovel/")
 @treat_proc_not_found
 @treat_proc_num_out_of_pattern
-def dados_responsavel(num_proc: str, json_alike: Optional[bool] = None):
+def dados_responsavel(num_proc: Optional[str] = None, json_alike: Optional[bool] = None):
+
+    print(f'***********{num_proc}************')
 
     if json_alike is None:
         json_alike = True
@@ -20,3 +23,20 @@ def dados_responsavel(num_proc: str, json_alike: Optional[bool] = None):
         p = get_proc_aleatorio()
 
     return dados_resps_imovel(p, json_alike = json_alike)
+
+@app.get("/metadados_dados_processo/")
+@treat_proc_not_found
+@treat_proc_num_out_of_pattern
+def metadados_processo(num_proc: Optional[str] = None, json_alike: Optional[bool] = None):
+
+    print(f'***********{num_proc}************')
+
+    if json_alike is None:
+        json_alike = True
+
+    if num_proc:
+        p = get_proc(num_proc)
+    else:
+        p = get_proc_aleatorio()
+
+    return get_proc_mdata(p, json_alike = json_alike)
