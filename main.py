90b22fec3517fe35d73eb99e93803fe4519e8d43
h-pgy <hpougy@gmail.com>
2021-03-03 20:48:03 -0300

    novo endpoint dados do terreno

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utilities.my_tools import get_proc_aleatorio, get_proc
 from utilities.proj_decorators import treat_proc_num_out_of_pattern, treat_proc_not_found
 from process_data.responsavel_imovel import dados_resps_imovel
 from process_data.metadados_processo import get_proc_mdata
+from process_data.dados_terreno import get_dados_terrenos
 
 app = FastAPI()
 
@@ -40,3 +41,20 @@ def metadados_processo(num_proc: Optional[str] = None, json_alike: Optional[bool
         p = get_proc_aleatorio()
 
     return get_proc_mdata(p, json_alike = json_alike)
+
+@app.get("/dados_terreno/")
+@treat_proc_not_found
+@treat_proc_num_out_of_pattern
+def dados_terrenos(num_proc: Optional[str] = None, json_alike: Optional[bool] = None):
+
+    print(f'***********{num_proc}************')
+
+    if json_alike is None:
+        json_alike = True
+
+    if num_proc:
+        p = get_proc(num_proc)
+    else:
+        p = get_proc_aleatorio()
+
+    return get_dados_terrenos(p, json_alike = json_alike)
