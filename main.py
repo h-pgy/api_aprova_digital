51b2f62f919846031fb6bcb4dd7edcadd71a0826
h-pgy <hpougy@gmail.com>
2021-04-16 12:44:08 -0300

    endpoint para listar processos por assunto

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from process_data.metadados_processo import get_proc_mdata, get_docs_published
 from process_data.dados_terreno import get_dados_terrenos
 from process_data.estande_de_vendas import dados_estandes_existentes, estandes_deferidos
 from process_data.apostilamento import apostilamentos_andamento
-from process_data.procs_por_assunto import list_assuntos
+from process_data.procs_por_assunto import list_assuntos, list_procs_by_assunto
 
 app = FastAPI()
 
@@ -131,3 +131,12 @@ def lista_de_assuntos(include_disabled : Optional[bool] = None, json_alike: Opti
         json_alike = True
 
     return list_assuntos(include_disabled = include_disabled, json_alike=json_alike)
+
+@app.get('/processos_por_assunto/')
+@encapsulate_response
+def lista_de_procs_por_assunto(assunto : str, json_alike: Optional[bool] = None):
+
+    if json_alike is None:
+        json_alike = True
+
+    return list_procs_by_assunto(assunto, json_alike=json_alike)
--- process_data/procs_por_assunto.py
+++ process_data/procs_por_assunto.py
@@ -1,3 +1,4 @@
+import re
 from utilities.my_tools import b_resp
 from utilities.proj_decorators import json_resp
 from utilities.connect import gen_db
@@ -35,5 +36,23 @@ def list_assuntos(*args, include_disabled = False, json_alike = True):
             )
             ]
         )
+
+    return dados
+
+@json_resp(list = True)
+def list_procs_by_assunto(assunto, *args, json_alike = True):
+
+    assunto = re.sub(r'^\d\. ', '', assunto)
+    assunto = assunto.strip()
+
+    db = gen_db()
+    procs = db.process.find({'config_metadata.title': assunto}, {'nP': 1})
+
+    dados = [
+        b_resp('num_protocolo',
+               'Número de protocolo do processo no Aprova Digital',
+               proc['nP'])
+        for proc in procs
+        ]
 
     return dados
